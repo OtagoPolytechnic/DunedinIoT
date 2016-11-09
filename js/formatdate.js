880dc101141061ccc4b37f7a9b75192f78c28100
Camper6 <sonshu.soush@gmail.com>
2016-11-10 12:41:24 +1300

    Tidying up

---
+++ js/formatdate.js
@@ -0,0 +1,8 @@
+$(function() {
+  $( "#datepicker-3" ).datepicker({
+    //appendText:"(yy-mm-dd)",
+    dateFormat:"d/m/y",
+    //altField: "#datepicker-4",
+    //altFormat: "DD, d MM, yy"
+  });
+});
